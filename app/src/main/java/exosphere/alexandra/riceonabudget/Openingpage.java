b8728db6421444c231fce8d48cca768f28cccc1e
luppo13 <luppo_13@hotmail.com>
2015-03-25 14:06:06 -0400

    Importing project
    Felt the need to save the code before destroying everything

---
+++ app/src/main/java/exosphere/alexandra/riceonabudget/Openingpage.java
@@ -0,0 +1,42 @@
+package exosphere.alexandra.riceonabudget;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Openingpage extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_openingpage);
+
+        startActivity(new Intent(Openingpage. this, Quickneasy.class));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_openingpage, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
